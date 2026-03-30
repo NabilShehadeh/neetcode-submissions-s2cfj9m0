cf294570b9a94cb29e554f7a9a386d50ce591f92
Nabil Shehadeh <57476301+NabilShehadeh@users.noreply.github.com>
2026-03-30 12:46:50 -0700

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sletters, tletters = {},{}
+        for letter in s:
+            sletters[letter] = sletters.get(letter, 0) +1
+        for letter in t:
+            tletters [letter]= tletters.get(letter, 0) +1
+        if sletters == tletters:
+            return True
+        return False
+        
