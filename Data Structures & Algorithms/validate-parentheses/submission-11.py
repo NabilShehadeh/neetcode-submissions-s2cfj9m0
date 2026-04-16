7d3b91bf0aa4e3eee239e6ec99f25dd59fee916d
Nabil Shehadeh <57476301+NabilShehadeh@users.noreply.github.com>
2026-04-15 20:32:57 -0700

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        matching = {')': '(', ']': '[', '}': '{'}
+        stack = []
+        # Logic behind the code:
+        # 1. is the parentheses open or closed?
+        # 2. if its open add it to the stack.
+        # 3. if its a closed parantheses we'll move on to more conditionals.
+        # 4. is the stack empty first, with no matching opening.
+        # 5. does the top parantheses match the current closing bracket. 
+        # 6. if so, then we'll remove that from the stack. 
+        # 7. by the end of the iteration we should see that we have an empty stack.
+        # 8. if our stack is essentially still containing some elements then its false.
+        # 9. since we have to have an empty stack by the end of everything.
+        for current in s:
+            if current in "[{(":
+                stack.append(current)
+            else:
+                if not stack:
+                    return False
+                if stack[-1] != matching[current]:
+                    return False
+                stack.pop()
+        return not stack
